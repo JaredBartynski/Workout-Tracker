c21574844fc08a459b6f7d175137778c7c614cff
JaredBartynski <70081376+JaredBartynski@users.noreply.github.com>
2020-10-22 21:21:26 -0400

    workout.js added workoutschema

--- models/workout.js
+++ models/workout.js
@@ -3,8 +3,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: Date,
-  exercises: Array,
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter exercise type.",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter excercise name.",
+      },
+      duration: {
+        type: Number,
+        required: "Enter the duration.",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
